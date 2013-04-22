d8f0db473076b85bd1db3f7cd50ac9a28310af07
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-21 22:38:26 -0400

    fixing bug in worker replacement

--- build/worker-replacement-one.js
+++ build/worker-replacement-one.js
@@ -1,5 +1,5 @@
 var workerSrc = {{src}};
-var blob = new Blob([workderSrc]);
+var blob = new Blob([workerSrc]);
 var blobUrl = window.URL.createObjectURL(blob);
 
 this.$worker = new Worker(blobUrl);
