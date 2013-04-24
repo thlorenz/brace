906f2bf3e4e11e0c746f19ab329d9f37465121e5
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-23 22:59:47 -0400

    using w3c-blob module to generate blob

--- build/worker-blob.js
+++ build/worker-blob.js
@@ -1,16 +1,6 @@
-    var blob;
     var workerSrc = mod.src;
-    var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder || window.MSBlobBuilder;
-
-    // Older, deprecated way of creating a blob
-    try {
-      blob = new BlobBuilder();
-      blob.append(workerSrc);
-      blob = blob.getBlob('application/javascript');
-    } catch (e) {
-      blob = new Blob([ workerSrc ], { type: 'application/javascript' });
-    }
-
+    var Blob = require('w3c-blob');
+    var blob = new Blob([ workerSrc ], { type: 'application/javascript' });
     var blobUrl = (window.URL || window.webkitURL).createObjectURL(blob);
 
     this.$worker = new Worker(blobUrl);
