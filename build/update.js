36a2fb6a76de6fb17c9849fbd059f14957657dcf
Thorsten Lorenz <thlorenz@gmx.de>
2014-01-28 12:54:59 -0500

    checking out specific ace tag instead of just using what is on master to update

--- build/update.js
+++ build/update.js
@@ -17,11 +17,14 @@ var workersrcdir =  path.join(braceroot, 'workersrc');
 var workerdir    =  path.join(braceroot, 'worker');
 var buildroot    =  path.join(__dirname, 'ace-build');
 
+var aceTag = 'v1.1.1';
+
 +function updateCleanAndPutInOrder() {
 
   +function cloneFreshAndRemoveUnneeded() {
     rm('-rf', buildroot) 
     exec('git clone git://github.com/ajaxorg/ace-builds.git ' + buildroot);
+    exec('(cd ' + buildroot + ' && git pull && git checkout ' + aceTag);
 
     [ 'demo', 'kitchen-sink', 'src-min-noconflict', 'src-min', 'src', 'textarea' ]
       .forEach(function (dir) { rm('-rf', path.join(buildroot, dir)) })
