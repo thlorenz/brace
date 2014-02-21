3e1c228f117610482f462cbbab7b90a5989862bb
Laurence Rowe <laurence@lrowe.co.uk>
2014-02-20 17:24:25 -0800

    Balance open parens in shell command

--- build/update.js
+++ build/update.js
@@ -24,7 +24,7 @@ var aceTag = 'v1.1.1';
   +function cloneFreshAndRemoveUnneeded() {
     rm('-rf', buildroot) 
     exec('git clone git://github.com/ajaxorg/ace-builds.git ' + buildroot);
-    exec('(cd ' + buildroot + ' && git pull && git checkout ' + aceTag);
+    exec('(cd ' + buildroot + ' && git pull && git checkout ' + aceTag + ')');
 
     [ 'demo', 'kitchen-sink', 'src-min-noconflict', 'src-min', 'src', 'textarea' ]
       .forEach(function (dir) { rm('-rf', path.join(buildroot, dir)) })
