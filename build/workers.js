9bb89206cf2d628f653b4f7cf3c1498b2dfa2d92
Jed Foster <jed@jedfoster.com>
2015-02-28 21:06:47 -0800

    Remove errant line

--- build/workers.js
+++ build/workers.js
@@ -3,7 +3,6 @@
 var workers = module.exports =  { 
     coffee     :  [ 'coffee' ]
   , css        :  [ 'css', 'html', 'luapage', 'markdown', 'rhtml' ]
-
   , html       :  [ 'coldfusion', 'curly', 'django', 'ejs', 'handlebars', 'html_ruby', 'html', 'luapage', 'markdown', 'php', 'rhtml', 'smarty', 'soy_template', 'twig', 'velocity' ]
   , javascript :  [ 'coldfusion', 'curly', 'django', 'groovy', 'html', 'java', 'javascript', 'luapage', 'markdown', 'rhtml', 'scala', 'svg', 'typescript' ]
   , json       :  [ 'json' ]
