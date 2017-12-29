3cf5f75b7cfd41b9e79b161b5bea22e2026ac311
bigmaoniu <joe.zhou@reactivecore.com>
2017-12-29 11:58:31 -0500

    version 10

--- build/workers.js
+++ build/workers.js
@@ -10,6 +10,7 @@ var workers = module.exports =  {
   , php        :  [ 'php' ]
   , xquery     :  [ 'xquery' ]
   , xml        :  [ 'xml', 'svg', 'markdown' ]
+  , trd        :  [ 'trd' ]
 };
 
 module.exports.supported = Object.keys(workers)
