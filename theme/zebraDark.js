688b1ba99ed77376ef893dddf75961908a5c234a
Dmitrii Boyarskikh <boyarskikh.ds@gmail.com>
2019-09-11 17:07:30 +0500

    Update zebraDark.js

--- theme/zebraDark.js
+++ theme/zebraDark.js
@@ -1,108 +1,133 @@
 ace.define("ace/theme/zebraDark",["require","exports","module","ace/lib/dom"], function(acequire, exports, module) {
 
-exports.isDark = false;
-exports.cssClass = "ace-zebraDark";
-exports.cssText = ".ace-zebraDark .ace_gutter {\
-background: #262626;\
-color: #fff\
+    exports.isDark = true;
+    exports.cssClass = "ace-zebraDark";
+    exports.cssText = ".ace-zebraDark .ace_gutter {\
+background: #3c3c3c;\
+color: rgba(255, 255, 255, 0.3)\
 }\
 .ace-zebraDark .ace_print-margin {\
 width: 1px;\
-background: #f6f6f6\
+background: #555651\
+}\
+.ace_gutter-cell.ace_error {\
+background-image: none;\
+background: #6b3838\
 }\
 .ace-zebraDark {\
-background-color: #fff;\
-color: #4D4D4C\
+background-color: #333333;\
+color: #F8F8F2;\
+font-size: 14px;\
+}\
+.ace-zebraDark .ace_scrollbar-v {\
+width: 7px !important;\
+bottom: 7px !important;\
+}\
+.ace-zebraDark .ace_scrollbar-h {\
+height: 7px !important;\
+right: 7px !important;\
+}\
+.ace-zebraDark .ace_scroller {\
+right: 0px !important;\
+bottom: 0px !important;\
 }\
 .ace-zebraDark .ace_cursor {\
-color: #AEAFAD\
+color: #F8F8F0\
 }\
 .ace-zebraDark .ace_marker-layer .ace_selection {\
-background: #D6D6D6\
+background: #49483E\
 }\
 .ace-zebraDark.ace_multiselect .ace_selection.ace_start {\
-box-shadow: 0 0 3px 0px #FFFFFF;\
+box-shadow: 0 0 3px 0px #272822;\
 }\
 .ace-zebraDark .ace_marker-layer .ace_step {\
-background: rgb(255, 255, 0)\
+background: rgb(102, 82, 0)\
 }\
 .ace-zebraDark .ace_marker-layer .ace_bracket {\
 margin: -1px 0 0 -1px;\
-border: 1px solid #D1D1D1\
+border: 1px solid #49483E\
 }\
 .ace-zebraDark .ace_marker-layer .ace_active-line {\
-background: #EFEFEF\
+background: #202020\
 }\
 .ace-zebraDark .ace_gutter-active-line {\
-background-color : #dcdcdc\
+background-color: #272727\
 }\
 .ace-zebraDark .ace_marker-layer .ace_selected-word {\
-border: 1px solid #D6D6D6\
+border: 1px solid #49483E\
 }\
 .ace-zebraDark .ace_invisible {\
-color: #D1D1D1\
-}\
-.ace-zebraDark .ace_keyword,\
-.ace-zebraDark .ace_meta,\
-.ace-zebraDark .ace_storage,\
-.ace-zebraDark .ace_storage.ace_type,\
-.ace-zebraDark .ace_support.ace_type {\
-color: #8959A8\
-}\
-.ace-zebraDark .ace_keyword.ace_operator {\
-color: #3E999F\
-}\
-.ace-zebraDark .ace_constant.ace_character,\
-.ace-zebraDark .ace_constant.ace_language,\
-.ace-zebraDark .ace_constant.ace_numeric,\
-.ace-zebraDark .ace_keyword.ace_other.ace_unit,\
-.ace-zebraDark .ace_support.ace_constant,\
-.ace-zebraDark .ace_variable.ace_parameter {\
-color: #F5871F\
-}\
-.ace-zebraDark .ace_constant.ace_other {\
-color: #666969\
-}\
-.ace-zebraDark .ace_invalid {\
-color: #FFFFFF;\
-background-color: #C82829\
-}\
-.ace-zebraDark .ace_invalid.ace_deprecated {\
-color: #FFFFFF;\
-background-color: #8959A8\
-}\
-.ace-zebraDark .ace_fold {\
-background-color: #4271AE;\
-border-color: #4D4D4C\
-}\
-.ace-zebraDark .ace_entity.ace_name.ace_function,\
-.ace-zebraDark .ace_support.ace_function,\
-.ace-zebraDark .ace_variable {\
-color: #4271AE\
-}\
-.ace-zebraDark .ace_support.ace_class,\
-.ace-zebraDark .ace_support.ace_type {\
-color: #C99E00\
-}\
-.ace-zebraDark .ace_heading,\
-.ace-zebraDark .ace_markup.ace_heading,\
-.ace-zebraDark .ace_string {\
-color: #718C00\
+color: #52524d\
 }\
 .ace-zebraDark .ace_entity.ace_name.ace_tag,\
-.ace-zebraDark .ace_entity.ace_other.ace_attribute-name,\
+.ace-zebraDark .ace_keyword,\
 .ace-zebraDark .ace_meta.ace_tag,\
-.ace-zebraDark .ace_string.ace_regexp,\
+.ace-zebraDark .ace_storage {\
+color: #F92672\
+}\
+.ace-zebraDark .ace_punctuation,\
+.ace-zebraDark .ace_punctuation.ace_tag {\
+color: #fff\
+}\
+.ace-zebraDark .ace_constant.ace_character,\
+.ace-zebraDark .ace_constant.ace_numeric,\
+.ace-zebraDark .ace_constant.ace_other {\
+color: #00CCBB\
+}\
+.ace-zebraDark .ace_constant.ace_language{\
+color: #0488E0\
+}\
+.ace-zebraDark .ace_invalid {\
+color: #F8F8F0;\
+background-color: #F92672\
+}\
+.ace-zebraDark .ace_invalid.ace_deprecated {\
+color: #F8F8F0;\
+background-color: #AE81FF\
+}\
+.ace-zebraDark .ace_support.ace_constant,\
+.ace-zebraDark .ace_support.ace_function {\
+color: #66D9EF\
+}\
+.ace-zebraDark .ace_fold {\
+background-color: #A6E22E;\
+border-color: #585858;\
+background-image: none;\
+position: relative;\
+background-color: rgba(255, 255, 255, 0);\
+}\
+.ace-zebraDark .ace_fold:after {\
+    content: '...';\
+    color: #fff;\
+    position: absolute;\
+    top: -7px;\
+}\
+.ace-zebraDark .ace_storage.ace_type,\
+.ace-zebraDark .ace_support.ace_class,\
+.ace-zebraDark .ace_support.ace_type {\
+font-style: italic;\
+color: #66D9EF\
+}\
+.ace-zebraDark .ace_entity.ace_name.ace_function,\
+.ace-zebraDark .ace_entity.ace_other,\
+.ace-zebraDark .ace_entity.ace_other.ace_attribute-name,\
 .ace-zebraDark .ace_variable {\
-color: #C82829\
+color: #fff\
+}\
+.ace-zebraDark .ace_variable.ace_parameter {\
+font-style: italic;\
+color: #FD971F\
+}\
+.ace-zebraDark .ace_string {\
+color: #FF809F\
 }\
 .ace-zebraDark .ace_comment {\
-color: #8E908C\
+color: #75715E\
 }\
 .ace-zebraDark .ace_indent-guide {\
-background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bdu3f/BwAlfgctduB85QAAAABJRU5ErkJggg==) right repeat-y\
+background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAEklEQVQImWPQ0FD0ZXBzd/wPAAjVAoxeSgNeAAAAAElFTkSuQmCC) right repeat-y\
 }";
 
-var dom = acequire("../lib/dom");
-dom.importCssString(exports.cssText, exports.cssClass);
+    var dom = acequire("../lib/dom");
+    dom.importCssString(exports.cssText, exports.cssClass);
 });
